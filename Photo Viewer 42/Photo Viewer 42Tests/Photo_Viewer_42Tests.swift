2db77bba17a500abb3bd32abebafd23404b7c8db
Gerardo Carlos Roderico Tan <gerardocarlosroderico.tan@qvcjp.com>
2019-11-26 11:07:25 +0900

    setup project

---
+++ Photo Viewer 42/Photo Viewer 42Tests/Photo_Viewer_42Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Photo_Viewer_42Tests.swift
+//  Photo Viewer 42Tests
+//
+//  Created by Gerardo Carlos Roderico Tan on 2019/11/26.
+//  Copyright © 2019 nytfury47. All rights reserved.
+//
+
+import XCTest
+@testable import Photo_Viewer_42
+
+class Photo_Viewer_42Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
